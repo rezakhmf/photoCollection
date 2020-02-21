d664cb9d078e6769c895a8d7c0475e7c8a5ab6ad
e214044 <yurii.petelko@evry.com>
2020-02-21 10:03:41 +0200

    Initial Commit

---
+++ PhotoLibrary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoLibrary
+//
+//  Created by Yurii Petelko on 2/21/20.
+//  Copyright © 2020 Squire. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
